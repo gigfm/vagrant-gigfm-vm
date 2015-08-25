8feee9a93d729f7b1dc6d8c492d5a1389c751903
uglymunky <gabriel@uglymunky.com>
2015-08-25 01:04:34 -0700

    add composer, bower, grunt, and busterjs

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -26,6 +26,7 @@ apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
     
 echo "Installing PHP extensions"
 apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
+curl -sS https://getcomposer.org/installer | php > /dev/null
 
 echo "Preparing MySQL"
 apt-get install debconf-utils -y > /dev/null
@@ -40,6 +41,16 @@ curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash - > /
 
 echo "Installing Node.js"
 apt-get install nodejs -y > /dev/null
+npm update -g npm
+
+echo "Install Bower"
+npm install -g bower > /dev/null
+
+echo "Install Grunt"
+npm install -g grunt-cli > /dev/null
+
+echo "Install BusterJs"
+npm install -g buster > /dev/null
 
 echo "Configuring Nginx"
 cp /vagrant/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
